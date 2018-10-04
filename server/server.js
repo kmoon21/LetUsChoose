c0a57b071a8c30a013c5f9ef3225c91e47830800
JohntPark <JohnTaeHeePark@gmail.com>
2018-10-04 10:11:15 -0700

    merge

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Routes = require('./routes/index')
 
 const app = express()
-const port = 3000
+const port = 4000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
